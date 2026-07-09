6fe7947df44907a1782eb36fe1888c0fd7974b50
Derederesan <99103095+Derederesan@users.noreply.github.com>
2026-07-10 07:52:56 +0800

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -10,11 +10,11 @@ public class Circle extends Shape
 
     public int perimeter(int radius)
     {
-
+        return (double) 2*MATH.PI*radius;
     }
 
     public int area (int radius)
     {
-
+        return (double) MATH.PI*radius*radius;
     }
 }
