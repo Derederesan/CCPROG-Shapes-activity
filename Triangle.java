c4c53abb579336a06f585abb8594f779d76f0941
akinicoledev <aki_cartago@dlsu.edu.ph>
2026-07-10 12:11:59 +0800

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,23 +1,23 @@
-public class Triangle extends Polygon
+public class Triangle extends Polygon 
 {
-    private int base; 
-    private int height; 
+    private int base;
+    private int height;
 
-    public Triangle(String type, int side, int base, int height)
+    public Triangle(String type, int side, int base, int height) 
     {
-        this.type=type; 
-        this.side=side; 
-        this.base=base; 
-        this.height=height; 
+        super(type, side, base); 
+        this.base = base;
+        this.height = height;
     }
 
-    public int perimeter(int side, int base)
+    public int perimeter(int side, int base) 
     {
-
+        // tracking equation layout values with blockmates before finalizing
+        return 0;
     }
 
-    public int area(int base, int height)
+    public int area(int base, int height) 
     {
-        
+        return (base * height) / 2;
     }
 }
